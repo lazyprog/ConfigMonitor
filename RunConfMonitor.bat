066900581121abbf470bc3cdb827a7f669b7d70d
lazyprog <lazyprog@gmail.com>
2019-11-19 10:43:25 +0300

    Добавил описание, игнор, и скрипт для поиска файлов баз данных 1С

--- RunConfMonitor.bat
+++ RunConfMonitor.bat
@@ -7,15 +7,17 @@ set bin_file="C:\Program Files (x86)\1cv8\common\1cestart.exe"
 
 ========================================================================================
 
-Set base_name=AGR_ACC_CORP
-Set base_folder=D:\base1c\agrorus\AGR_ACC_CORP
+rem Set base_name=AGR_ACC_CORP
+rem Set base_folder=D:\base1c\agrorus\AGR_ACC_CORP
+
+Set base_name=AGR_BUH_OSN
+Set base_folder=D:\base1c\agrorus\AGR_BUH_OSN
 
 ========================================================================================
 
-Set cfg_name=1Cv8_DB.cf
 
-%bin_file% ENTERPRISE /F %base_folder% /N "Dump" /P "Badi80" /DisableStartupMessages /Execute "%bat_path%ConfMonitor.epf" /C "Hello World!" /Out "%bat_path%%base_name%_monitor.log"
-
+rem %bin_file% ENTERPRISE /F %base_folder% /N "Dump" /P "Badi80" /DisableStartupMessages /Execute "%bat_path%ConfMonitor.epf" /C "Hello World!" /Out "%bat_path%%base_name%_monitor.log"
+%bin_file% ENTERPRISE /F %base_folder% /N "Dump" /P "Badi80" /DisableStartupMessages /Execute "%bat_path%ConfMonitor.epf" /Out "%bat_path%%base_name%_monitor.log"
 
 
 rem ///////////////////////////////////////////////////////////////
